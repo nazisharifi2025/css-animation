8a3751055c7b5e151fa7ede2d50a972025f54e64
nazisharifi2025 <banonazisharifi@gmail.com>
2025-03-16 19:55:23 +0430

    added first

---
+++ first/app.js
@@ -0,0 +1,16 @@
+let arr=[0,9,8,7,6,5,4,3];
+for(let i=0;i<arr.length;i++){
+    console.log(arr[i]);
+}
+console.log("-------");
+for(let b of arr){
+    console.log(b);
+}
+let item=[26,21,42,55,44,63,74];
+for(let loop of item){
+    console.log(loop);
+}
+let arrey1=[];
+for(let elemint =0;elemint<7;elemint++){
+    console.log(elemint)
+}
