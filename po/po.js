ef8220b6e16beb02343e522f00f38a77bbfb15ad
nazisharifi2025 <banonazisharifi@gmail.com>
2025-03-23 19:59:35 +0430

    d

--- po/po.js
+++ po/po.js
@@ -1,33 +1,33 @@
-function hi(mb){
-    return (6/9) * (mb-30);
-}
-console.log(hi(36));
-console.log("let-number");
-let a=10;
-a="ali";
-console.log("10");
-console.log("const-string");
-const b=12;
-console.log("ali");
-console.log("objict");
-let propirti ={
-    name:"Compiuter",
-    Bettri:"lind",
-    Size:10,
+// function hi(mb){
+//     return (6/9) * (mb-30);
+// }
+// console.log(hi(36));
+// console.log("let-number");
+// let a=10;
+// a="ali";
+// console.log("10");
+// console.log("const-string");
+// const b=12;
+// console.log("ali");
+// console.log("objict");
+// let propirti ={
+//     name:"Compiuter",
+//     Bettri:"lind",
+//     Size:10,
     
 
-}
-// console.log(propirti.Bettri);
-// let n=0;
-// while(n<6){
-//     console.log("no");
-//     i++ ;
 // }
-let t=0;
-do{
-    console.log("biby");
-}
-while(t<20);
+// // console.log(propirti.Bettri);
+// // let n=0;
+// // while(n<6){
+// //     console.log("no");
+// //     i++ ;
+// // }
+// let t=0;
+// do{
+//     console.log("biby");
+// }
+// while(t<20);
 let nine={
  name:"nazi",
  FatherName:"Hadi",
@@ -35,4 +35,6 @@ let nine={
     Age:18,
     birthdey:2006,
 }
-console.log(nine[Age]);
+for(let key in nine){
+    console.log(key)
+}
