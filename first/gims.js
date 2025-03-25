42735550681b6fcd3adc6ebc0f18c237bb228642
nazisharifi2025 <banonazisharifi@gmail.com>
2025-03-25 21:07:26 +0430

    add

---
+++ first/gims.js
@@ -0,0 +1,6 @@
+// const startBtn =document.getElementById("startGames");
+// function start(){
+//     // console.log("Gem is started....");
+//     prompt()
+// }
+// startBtn.addEventListener("click",start);
