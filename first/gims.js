f5335e08015ab7a8d039ed555cb25d6455100b6e
nazisharifi2025 <banonazisharifi@gmail.com>
2025-03-25 21:20:23 +0430

    h

--- first/gims.js
+++ first/gims.js
@@ -1,8 +1,15 @@
 const startBtn =document.getElementById("startGames");
+let computerChoice="ROCK";
+const roke = "ROCK";
+const SCISSORS="SCESSORS";
+const PAPER="PAPER";
+ 
 function start(){
     // console.log("Gem is started....");
    let playerChoice= prompt("ROCK , PAPER OR SCISSORS?");
    let  randomNumber=Math.random();
-   
+   if(randomNumber<0.33){
+
+   }
 }
 startBtn.addEventListener("click",start);
