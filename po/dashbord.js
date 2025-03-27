2ec2c6334a2e285dc08283040a698a546156e3f5
nazisharifi2025 <banonazisharifi@gmail.com>
2025-03-27 21:02:28 +0430

    l

---
+++ po/dashbord.js
@@ -0,0 +1,1 @@
+// const horizontaINav =document.getElementById("hnav");
--- po/po.js
+++ po/po.js
@@ -1,1 +1,21 @@
-// 
+let greeting =(a)=>{
+    console.log(`Hi dear ${a}`);
+}
+greeting("ali");
+function data(a=10,b=12){
+    let result=a*b;
+    console.log(result);
+}
+data();
+function namea (n,f=19){
+    console.log(n/f);
+}
+namea(14);
+let hi = (...g)=>{ 
+    let h=0;
+    for(let i of g){
+        h +=i;
+    }
+    return h;
+}
+console.log(hi(1,2,34,54,65,1));
