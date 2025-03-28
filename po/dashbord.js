45e3bcf4f5781b6ee8d5ed30e104a96be319c17a
nazisharifi2025 <banonazisharifi@gmail.com>
2025-03-28 22:27:45 +0430

    added

--- po/dashbord.js
+++ po/dashbord.js
@@ -2,10 +2,10 @@ const horizontaINav =document.getElementById("hnav");
 const verticalNav = document.getElementById("vnav");
 const bars = document.getElementById("bars");
 const close = document.getElementById("close");
-// bars.addEventListener("click" ,()=>{
-//     horizontaINav.classList.add("w-[75%]");
-//     horizontaINav.classList.remove("w-full");
-//     verticalNav.classList.remove("hidden");
-//     bars.style.display="none";
-//     close.style.display="block";
-// })
+bars.addEventListener("click" ,()=>{
+    horizontaINav.classList.add("w-[75%]");
+    horizontaINav.classList.remove("w-full");
+    verticalNav.classList.remove("hidden");
+    bars.style.display="none";
+    close.style.display="block";
+})
