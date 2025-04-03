dfacc32601b4861fc5f8b078373db3c0bd4e5775
nazisharifi2025 <banonazisharifi@gmail.com>
2025-04-03 10:45:20 +0430

    l

--- po/dashbord.js
+++ po/dashbord.js
@@ -4,15 +4,15 @@ const bars = document.getElementById("bars");
 const close = document.getElementById("close");
 bars.addEventListener("click" ,()=>{
     horizontaINav.classList.add("w-[75%]");
-    horizontaINav.classList.remove("w-full");
-    verticalNav.classList.remove("hidden");
+    horizontaINav.classList.remove("w-[95%]");
+    verticalNav.classList.remove("ml-[-10%]");
     bars.style.display="none";
     close.style.display="block";
 });
 close.addEventListener("click",()=>{
-    horizontaINav.classList.add("w-full");
+    horizontaINav.classList.add("w-[95%]");
     horizontaINav.classList.remove("w-[75%]");
-    verticalNav.classList.add("hidden");
+    verticalNav.classList.add("ml-[-10%]");
     bars.style.display="block";
     close.style.display="none";
 });
