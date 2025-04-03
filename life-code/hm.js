d60521419c4568ac18e20b2f47d5a0467935a331
nazisharifi2025 <banonazisharifi@gmail.com>
2025-04-03 20:43:28 +0430

    l

---
+++ life-code/hm.js
@@ -0,0 +1,16 @@
+function updateClock(){
+    const now = new Date();
+    const hours = now.getHours() % 12;
+    const minutes = now.getMinutes();
+    const seconds = now.getSecounds();
+
+    const hourDeg = (hours * 30) + (minutes * 0.5);
+    const minuteDeg = (minutes * 6) + (seconds * 0.1);
+    const secondDeg = seconds * 6;
+
+    document.getElementById("hours").style.transform = `translateX(-50%) rotate(${hourDeg}deg)`;
+    document.getElementById("minute").style.transform = `translateX(-50%) rotate(${minuteDeg}deg)`;
+    document.getElementById("second").style.transform = `translateX(-50%) rotate(${secondDeg}deg)`;
+}
+setInterval(updateClock, 1000);
+updateClock();
