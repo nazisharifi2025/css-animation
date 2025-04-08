31f519ea0772b36cd20601feb362aaa828a5c9a1
nazisharifi2025 <banonazisharifi@gmail.com>
2025-04-08 19:52:09 +0430

    l

--- javascript-contint/java.js
+++ javascript-contint/java.js
@@ -6,5 +6,5 @@ h1.parentNode.appendChild(newElement);
 const p = document.getElementsByTagName("p").item(0);
 const newText2 = document.createTextNode("Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iste sed minus delectus eius perferendis alias? Ut deserunt eum nesciunt, velit, quo aspernatur dignissimos consectetur recusandae cupiditate placeat optio, harum non!");
 const newElement2 = document.createElement("p");
-// newElement2.appendChild(newText2);
+newElement2.appendChild(newText2);
 // p.parentNode.appendChild(newElement2);
