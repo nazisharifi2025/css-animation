3ebf01785b87e843b833c54157675771d646ac1a
nazisharifi2025 <banonazisharifi@gmail.com>
2025-04-08 19:44:42 +0430

    l

--- javascript-contint/java.js
+++ javascript-contint/java.js
@@ -2,4 +2,4 @@ const h1 = document.getElementsByTagName("h1").item(0);
 const newText = document.createTextNode("in the name of allah");
 const newElement = document.createElement("h1");
 newElement.appendChild(newText);
-// h1.parentNode.appendChild(newElement);
+h1.parentNode.appendChild(newElement);
