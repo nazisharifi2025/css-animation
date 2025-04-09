59b74a501c440467079dc00086f524b4ae4c0231
nazisharifi2025 <banonazisharifi@gmail.com>
2025-04-09 21:13:44 +0430

    l

--- Dome/dome.js
+++ Dome/dome.js
@@ -1,7 +1,7 @@
 const body = document.body;
 function generateTable(){
     const nTable = document.createElement("table");
-    for(let i; i<3; i++){
+    for(let i = 0; i<3; i++){
         const nTr = document.createElement("tr");  
         for(let j = 0; j<5; j++){
             const nText = document.createTextNode("Naz");
@@ -9,8 +9,8 @@ function generateTable(){
             nTd.appendChild(nText);
             nTr.appendChild(nTd);
         }
-        nTr.appendChild(nTable);
-        nTr.setAttribute("border","2");
+        nTable.appendChild(nTr);
+        nTable.setAttribute("border","2");
     }
     body.appendChild(nTable);
 }
