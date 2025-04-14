7c0f9c50a7b49b4705d876c66b85e669d520e714
nazisharifi2025 <banonazisharifi@gmail.com>
2025-04-14 10:14:37 +0430

    l

---
+++ Dashbord/Dashbord.js
@@ -0,0 +1,25 @@
+const horizontaINav = document.getElementById("hnav");
+const verticalNav = document.getElementById("vnav");
+const bars = document.getElementById("bars");
+const close = document.getElementById("close");
+const hid = document.getElementById("hid");
+const hideen = document.getElementById("hideen");
+bars.addEventListener("click", () => {
+  if (horizontaINav.classList.contains("w-[100%]")) {
+    horizontaINav.classList.remove("w-[100%]");
+    horizontaINav.classList.add("w-[85%]");
+    verticalNav.classList.remove("ml-[-10%]");
+    hid.classList.remove("hidden");
+    hid.classList.add("flex");
+    hideen.classList.remove("hidden");
+    hideen.classList.add("flex");
+  } else {
+    horizontaINav.classList.add("w-[100%]");
+    horizontaINav.classList.remove("w-[85%]");
+    verticalNav.classList.add("ml-[-10%]");
+    hid.classList.add("hidden");
+    hid.classList.remove("flex");
+    hideen.classList.remove("flex");
+    hideen.classList.add("hidden");
+  }
+});
